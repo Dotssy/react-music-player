665b29b1814e2531a44f68c02b708f74efcd3746
Bohdan Zozulya <zozulya.bohdan@gmail.com>
2024-12-11 16:27:53 +0200

    Added background and title styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AudioPlayer } from './components/AudioPlayer';
 function App() {
   return (
     <div className="mx-auto max-w-[1200px]">
-      <h1 className="text-3xl text-center font-bold mb-5 text-gray-600">
+      <h1 className="text-4xl text-center font-bold mb-5 mt-5 text-gray-600">
         Audio player in React
       </h1>
       <AudioPlayer />
--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -11,7 +11,7 @@ export const AudioPlayer = () => {
 
   return (
     <div>
-      <div className="min-h-8 bg-[#2e2d2d] flex flex-col gap-9 lg:flex-row justify-between items-center text-white p-[0.5rem_10px]">
+      <div className="min-h-8 bg-[#2e2d2d] flex flex-col gap-9 lg:flex-row justify-between items-center text-white p-[0.5rem_10px] rounded">
         <TrackInfo />
         <div className="w-full flex flex-col items-center gap-1 m-auto flex-1">
           <Controls />
